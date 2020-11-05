dfdbc311648938c642913b817b1d03c326d11138
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2020-11-05 14:10:36 -0300

    reset de cores no log

--- exemplos/01-fundamentos-de-testes/04-aplicando-testes-unitarios-em-uma-cli/src/utils/logger.js
+++ exemplos/01-fundamentos-de-testes/04-aplicando-testes-unitarios-em-uma-cli/src/utils/logger.js
@@ -1,7 +1,11 @@
+const RESET = '\x1b[0m';
+const TEXT = '%s';
+const resetAfterLog = color => `${color}${TEXT}${RESET}`;
+
 export const COLORS = {
-  DEFAULT: "\x1b[0m",
-  GREEN: "\x1b[32m",
-  RED: "\x1b[31m"
+  DEFAULT: resetAfterLog('\x1b[0m$'),
+  GREEN: resetAfterLog('\x1b[32m'),
+  RED: resetAfterLog('\x1b[31m')
 };
 
 const log = (msg, ...args) => {
