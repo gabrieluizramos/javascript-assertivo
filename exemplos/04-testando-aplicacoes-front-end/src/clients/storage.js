229a0041e63aea681b7dd85323b61ea8662e609b
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2020-11-18 16:58:48 -0300

    ajusta cliente de storage

--- exemplos/04-testando-aplicacoes-front-end/src/clients/storage.js
+++ exemplos/04-testando-aplicacoes-front-end/src/clients/storage.js
@@ -1,9 +1,10 @@
-const STORAGE_KEY = 'user';
+export const STORAGE_KEY = 'user';
+const baseClient = localStorage;
 
-export const getData = ({ client = localStorage } = {}) => JSON.parse(client.getItem(STORAGE_KEY));
-export const setData = ({ client = localStorage, data } = {}) => client.setItem(STORAGE_KEY, JSON.stringify(data));
-export const removeData = ({ client = localStorage } = {}) => client.removeItem(STORAGE_KEY);
-export const hasData = () => !!getData();
+export const getData = ({ client = baseClient } = {}) => JSON.parse(client.getItem(STORAGE_KEY));
+export const setData = ({ client = baseClient, data = {} } = {}) => client.setItem(STORAGE_KEY, JSON.stringify(data));
+export const removeData = ({ client = baseClient } = {}) => client.removeItem(STORAGE_KEY);
+export const hasData = (opts) => !!getData(opts);
 
 export const clearCookie = ({ client }) => {
   const past = new Date(0, 0, 0)
