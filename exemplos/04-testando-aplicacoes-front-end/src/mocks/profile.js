5abef23653c93cfd6d38c9db0caf0b1f79b537f6
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2020-11-25 16:22:33 -0300

    ajusta imagem que estava dando erro 403

--- exemplos/04-testando-aplicacoes-front-end/src/mocks/profile.js
+++ exemplos/04-testando-aplicacoes-front-end/src/mocks/profile.js
@@ -3,7 +3,7 @@ export const profile = {
   userName: 'Donna.Watsica38',
   name: 'Hipolito',
   lastName: 'Kassulke',
-  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/iamgarth/128.jpg',
+  avatar: 'https://avatars0.githubusercontent.com/u/13336905?s=460&u=2e6422672bb3f2aeaaa034e9605e953b5bbf49ca&v=4',
   role: 'USER',
   uid: '1234567-abc',
 };
